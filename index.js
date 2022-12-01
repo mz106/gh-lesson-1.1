86804ce4c71fb898faac8e699f3da55951fa1514
Michael Z <michaelzwierzanski@gmail.com>
2022-12-01 14:00:53 +0000

    add if block printFruit

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const fruitArr = ["apple", "pear"];
 
 const printFruit = (arr) => {
   for (let i = 0; i < arr.length; i++) {
-    console.log(`I like ${arr[i]}s`);
+    if (arr[i] === "apple") {
+      console.log("I like apples");
+    } else {
+      console.log(`I don't like ${arr[i]}s`);
+    }
   }
 };
 
