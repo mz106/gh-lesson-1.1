fb56e3ab929ba488a531350cbc8cffd312b5ec71
Michael Z <michaelzwierzanski@gmail.com>
2022-12-01 13:46:32 +0000

    for loop add to printFruit func

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fruitArr = ["apple", "pear"];
 
-const printFruit = () => {
-  console.log("I like apples");
-  console.log("I like pears");
+const printFruit = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`I like ${arr[i]}s`);
+  }
 };
 
 printFruit();
